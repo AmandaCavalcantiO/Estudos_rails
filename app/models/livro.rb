32b3211c41a2757e17165252111c151b4293f8aa
AmandaC <amanda.cavalcanti06@gmail.com>
2022-04-05 16:57:15 -0300

    Adicionado filtro para os livros e paginação

--- app/models/livro.rb
+++ app/models/livro.rb
@@ -3,4 +3,8 @@ class Livro < ActiveRecord::Base
   has_many :comentarios, dependent: :destroy
   validates :autor, presence: true
   validates :nome, presence: true
+
+  scope :leitura_concluida, -> { where(concluido: true) }
+  scope :leitura_pendente, -> { where(concluido: false) }
+
 end
